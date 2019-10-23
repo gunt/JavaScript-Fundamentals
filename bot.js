9e41ab7181048d74ec9daafd8e1b633ba927bcf2
gunt <4261629+gunt@users.noreply.github.com>
2019-10-23 15:40:43 +0200

    change the url

--- bot.js
+++ bot.js
@@ -86,3 +86,7 @@ var retweet = function () {
 };
 
 retweet();
+
+/ Heroku + node.js error (Web process failed to bind to $PORT within 60 seconds of launch)
+// https://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
+app.listen(process.env.PORT || 5000);
